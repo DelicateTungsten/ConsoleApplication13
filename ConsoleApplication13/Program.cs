c6d4fd30ecf235a7879a426390d9c027b29597b3
DelicateTungsten <shubnit.bhumbra@systemc.com>
2017-01-11 15:31:39 +0000

    MAT-1896
    Initial test

---
+++ ConsoleApplication13/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace ConsoleApplication13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = KanyeInterruptBuilder("C#", "Java", "immutable parameters");
+            Console.WriteLine(text);
+            Console.ReadKey();
+        }
+
+        static string KanyeInterruptBuilder(string winner, string runnerUp, string talent)
+        {
+            return string.Format(
+                "Yo {0}, I'm really happy for you, I'ma let you finish...{1}...but {2} had one of the best {3} of all time...{1}one of the best {3} of all time.",
+                winner,
+                Environment.NewLine,
+                runnerUp,
+                talent);
+        }
+    }
+}
